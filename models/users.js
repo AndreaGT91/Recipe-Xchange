17781c69d3f560ee06d15963cc8cbfc6d9718c42
Katherine Jones <kejone1414@gmail.com>
2020-06-08 19:21:28 -0400

    feat: added sequelize model users.js

---
+++ models/users.js
@@ -0,0 +1,46 @@
+const bcrypt = require("bcryptjs");
+
+module.exports = function (sequelize, DataTypes) {
+    const Users = sequelize.define("Users", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        aboutMe: DataTypes.TEXT,
+        imperial: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true,
+        },
+    });
+
+    Users.prototype.validPassword = function (password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+
+    Users.addHook("beforeCreate", function (user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
+
+    return Users;
+};
