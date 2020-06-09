1ff3545111e72886439aaf5bd4508e4c4da12abe
Katherine Jones <kejone1414@gmail.com>
2020-06-08 21:06:41 -0400

    feat: updated server.js to add session and passport lines

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passport");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(PORT, function() { console.log("App listening on PORT " + PORT); });
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+app.listen(PORT, function () { console.log("App listening on PORT " + PORT); });
+
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () { console.log("App listening on PORT " + PORT); });
+});
