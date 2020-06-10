a54884fe2411361613735c3aec3ca81c7483df54
Katherine Jones <kejone1414@gmail.com>
2020-06-10 19:31:02 -0400

    feat: added recipe routes

--- routes/recipe-api-routes.js
+++ routes/recipe-api-routes.js
@@ -1,10 +1,27 @@
 const db = require("../models");
 
 module.exports = function (app) {
+    app.get("/api/recipes", function (req, res) {
+        db.Recipes.findAll({}).then(function (dbRecipes) {
+            res.json(dbRecipes);
+        });
+    });
+
+    app.get("/api/recipes/:id", function (req, res) {
+        db.Recipes.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbRecipe) {
+            res.json(dbRecipe);
+        });
+    });
+
     app.post("/api/addrecipe", function (req, res) {
         db.Recipes.create({
             title: req.body.title,
             source: req.body.source,
+            description: req.body.description,
             prepTime: req.body.prepTime,
             cookTime: req.body.cookTime,
             ovenTempF: req.body.ovenTempF,
@@ -15,13 +32,35 @@ module.exports = function (app) {
             category2: req.body.category2,
             category3: req.body.category3,
             public: req.body.public,
-            userID: req.body.userID
+            UserId: req.body.UserId
         })
-            .then(function () {
-                // What do we want to do after they add the recipe?
+            .then(function (dbRecipe) {
+                res.json(dbRecipe)
             })
             .catch(function (err) {
                 res.status(401).json(err);
             })
+    });
+
+    app.put("/api/recipes", function (req, res) {
+        db.Recipes.update(
+            req.body,
+            {
+                where: {
+                    id: req.body.id
+                }
+            }).then(function (dbRecipe) {
+                res.json(dbRecipe);
+            });
+    });
+
+    app.delete("/api/recipes/:id", function (req, res) {
+        db.Recipes.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbRecipe) {
+            res.json(dbRecipe);
+        });
     });
 };
